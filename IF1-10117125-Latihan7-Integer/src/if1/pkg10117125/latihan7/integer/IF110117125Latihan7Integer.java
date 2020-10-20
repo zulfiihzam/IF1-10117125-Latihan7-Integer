87d536da3ab96c4d34fc5673d60daf2bca459e8d
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:39:10 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan7-Integer/src/if1/pkg10117125/latihan7/integer/IF110117125Latihan7Integer.java
@@ -0,0 +1,33 @@
+/*
+ * 
+ * @author
+ * NAMA : Zulfi Ihzam R
+ * KELAS : PBO1
+ * NIM : 10117125
+ */
+
+package if1.pkg10117125.latihan7.integer;
+
+
+public class IF110117125Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 900000000000L;
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+    }
+
+}
